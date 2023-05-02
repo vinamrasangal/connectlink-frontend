1db520a3f235dd88f3662b85fb4726c1bd4338eb
Malik-kajiji <101195566+Malik-kajiji@users.noreply.github.com>
2023-05-02 10:55:28 +0200

    updated the create post component to the latest UIdesign

--- nestedComponents/CreatePost.js
+++ nestedComponents/CreatePost.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from 'react';
 import styles from '../styles/createPost.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { CreatePostActions } from '../redux/showCreatePost';
+import DropImage from './DropImage';
+import { FiUpload } from 'react-icons/fi';
 
 const CreatePost = () => {
-    const isShowen = useSelector(state => state.createPostController.showen)
+    const isShowen = useSelector(state => state.createPostController.showen);
+    const [image,setImage] = useState(null);
+    const [imageUrl,setImageUrl] = useState(null);
     const [current,setCurrent] = useState(1);
     const dispatch = useDispatch();
     function handleCancel(e){
@@ -19,7 +23,7 @@ const CreatePost = () => {
                     <li className={`${styles.li} ${current === 1? styles.active :''}`} onClick={()=>setCurrent(1)}>Share Your Idea</li>
                     <li className={`${styles.li} ${current === 2? styles.active :''}`} onClick={()=>setCurrent(2)}>Ask Question</li>
                     <li className={`${styles.li} ${current === 3? styles.active :''}`} onClick={()=>setCurrent(3)}>Discussion</li>
-                    <li className={`${styles.li} ${current === 4? styles.active :''}`} onClick={()=>setCurrent(4)}>Custom</li>
+                    <li className={`${styles.li} ${current === 4? styles.active :''}`} onClick={()=>setCurrent(4)}>Post</li>
                 </ul>
                 <div 
                 className={`${styles.formsHolder} ${current === 1?styles.first:''} ${current === 2?styles.second:''} ${current === 3?styles.third:''} ${current === 4?styles.fourth:''}`}
@@ -43,13 +47,37 @@ const CreatePost = () => {
                         <select className={`${styles.input} medium-fs light normal-gray`} id='industry' >
                             <option value="">Select the industry </option>
                         </select>
-                        <div className={styles.rolesAndTags}>
-                            <label htmlFor="" className={`${styles.RolesLable} small-fs normal dark-gray`}>Required Roles</label>
-                            <input className={`${styles.input} ${styles.RolesInput} medium-fs light normal-gray`} placeholder='tell us more' type="text" />
-                            <label htmlFor="" className={`${styles.tagsLabel} small-fs normal dark-gray`}>tags</label>
-                            <select className={`${styles.input} ${styles.tagsInput} medium-fs light normal-gray`} id='industry'>
-                                <option value="">Select relevant tags </option>
-                            </select>
+                        <label className='small-fs normal dark-gray' htmlFor="desc">Tags</label>
+                        <input 
+                            className={`${styles.input} medium-fs light normal-gray`}
+                            type="text" 
+                            id='desc' 
+                            placeholder='Enter tags e.g #edtech, #produtivity' 
+                        />
+                        <div className={styles.add}>
+                            <label className={`${styles.linksLabel} small-fs normal dark-gray`} htmlFor="desc">Add links</label>
+                            <input 
+                                className={`${styles.input} ${styles.linksInput} medium-fs light normal-gray`}
+                                type="text" 
+                                id='desc' 
+                                placeholder='Add relevant links e.g articles/websites' 
+                            />
+                            <label className={`${styles.imageLabel} small-fs normal dark-gray`} htmlFor="desc">Add image</label>
+                            <DropImage setAcceptedFile={setImage} setUrl={setImageUrl}>
+                                <button className={`${styles.upload} S-BTN`}>
+                                    {FiUpload({})} Upload
+                                </button>
+                            </DropImage>
+                        </div>
+                        <div className={styles.checkBoxes}>
+                            <input type='checkbox' className={`${styles.checkbox} checkbox `} />
+                            <label className={`${styles.label} small-fs normal dark-gray`} htmlFor="desc">Allow Discussion</label>
+                            <p className={`${styles.p} small-fs light normal-gray `}>Start a discussion and get valuable insights from the community on your idea.</p>
+                        </div>
+                        <div className={styles.checkBoxes}>
+                            <input type='checkbox' className={`${styles.checkbox} checkbox `} />
+                            <label className={`${styles.label} small-fs normal dark-gray`} htmlFor="desc">Hire Requirement</label>
+                            <p className={`${styles.p} small-fs light normal-gray`}>Specify the roles you need to bring your idea to life. Find the perfect fit for your team and start collaborating today.</p>
                         </div>
                         <div className={styles.btns}>
                             <button className={`${styles.cancelBtn} S-BTN`} onClick={(e)=>handleCancel(e)}>Cancel</button>
@@ -57,96 +85,68 @@ const CreatePost = () => {
                         </div>
                     </form>
                     <form action="" className={styles.form}>
-                        <label  htmlFor="title">Idea Title</label>
+                        <label className='small-fs normal dark-gray'  htmlFor="title">Question</label>
                         <input 
-                            className={styles.input}
+                            className={`${styles.input} medium-fs light normal-gray`}
                             type="text" 
                             id='title' 
-                            placeholder='Briefly describe your idea in a few words' 
+                            placeholder='What do you want to ask?' 
                         />
-                        <label  htmlFor="desc">Idea Description</label>
+                        <label className='small-fs normal dark-gray' htmlFor="desc">Description</label>
                         <input 
-                            className={styles.input}
+                            className={`${styles.input} medium-fs light normal-gray`}
                             type="text" 
                             id='desc' 
                             placeholder='Tell us more about your idea and how you envision it coming to life' 
                         />
-                        <label htmlFor="industry">Industry</label>
-                        <select className={styles.input} id='industry' >
-                            <option value="">Select the industry </option>
-                        </select>
-                        <div className={styles.rolesAndTags}>
-                            <label htmlFor="" className={styles.RolesLable}>Required Roles</label>
-                            <input className={`${styles.input} ${styles.RolesInput}`} placeholder='tell us more' type="text" />
-                            <label htmlFor="" className={styles.tagsLabel}>tags</label>
-                            <select className={`${styles.input} ${styles.tagsInput}`} id='industry'>
-                                <option value="">Select relevant tags </option>
-                            </select>
-                        </div>
+                        <label className='small-fs normal dark-gray' htmlFor="industry">Tags</label>
+                        <input 
+                            className={`${styles.input} medium-fs light normal-gray`}
+                            type="text" 
+                            id='desc' 
+                            placeholder='Enter tags e.g #edtech, #produtivity' 
+                        />
+                        
+
                         <div className={styles.btns}>
                             <button className={`${styles.cancelBtn} S-BTN`}>Cancel</button>
                             <button className={`${styles.postBtn} P-BTN`}>Post Idea</button>
                         </div>
                     </form>
                     <form action="" className={styles.form}>
-                        <label  htmlFor="title">Idea Title</label>
+                    <label className='small-fs normal dark-gray'  htmlFor="title">Title</label>
                         <input 
-                            className={styles.input}
+                            className={`${styles.input} medium-fs light normal-gray`}
                             type="text" 
                             id='title' 
-                            placeholder='Briefly describe your idea in a few words' 
+                            placeholder="What's on your mind?"
                         />
-                        <label  htmlFor="desc">Idea Description</label>
+                        <label className='small-fs normal dark-gray' htmlFor="desc">Description</label>
                         <input 
-                            className={styles.input}
+                            className={`${styles.input} medium-fs light normal-gray`}
                             type="text" 
                             id='desc' 
-                            placeholder='Tell us more about your idea and how you envision it coming to life' 
+                            placeholder='Tell us more about your topic' 
                         />
-                        <label htmlFor="industry">Industry</label>
-                        <select className={styles.input} id='industry' >
-                            <option value="">Select the industry </option>
-                        </select>
-                        <div className={styles.rolesAndTags}>
-                            <label htmlFor="" className={styles.RolesLable}>Required Roles</label>
-                            <input className={`${styles.input} ${styles.RolesInput}`} placeholder='tell us more' type="text" />
-                            <label htmlFor="" className={styles.tagsLabel}>tags</label>
-                            <select className={`${styles.input} ${styles.tagsInput}`} id='industry'>
-                                <option value="">Select relevant tags </option>
-                            </select>
-                        </div>
+                        <label className='small-fs normal dark-gray' htmlFor="industry">Tags</label>
+                        <input 
+                            className={`${styles.input} medium-fs light normal-gray`}
+                            type="text" 
+                            id='desc' 
+                            placeholder='Enter tags e.g #edtech, #produtivity' 
+                        />
                         <div className={styles.btns}>
                             <button className={`${styles.cancelBtn} S-BTN`}>Cancel</button>
                             <button className={`${styles.postBtn} P-BTN`}>Post Idea</button>
                         </div>
                     </form>
                     <form action="" className={styles.form}>
-                        <label  htmlFor="title">Idea Title</label>
-                        <input 
+                        <textarea 
                             className={styles.input}
                             type="text" 
                             id='title' 
-                            placeholder='Briefly describe your idea in a few words' 
+                            placeholder='What would you like to share today?' 
                         />
-                        <label  htmlFor="desc">Idea Description</label>
-                        <input 
-                            className={styles.input}
-                            type="text" 
-                            id='desc' 
-                            placeholder='Tell us more about your idea and how you envision it coming to life' 
-                        />
-                        <label htmlFor="industry">Industry</label>
-                        <select className={styles.input} id='industry' >
-                            <option value="">Select the industry </option>
-                        </select>
-                        <div className={styles.rolesAndTags}>
-                            <label htmlFor="" className={styles.RolesLable}>Required Roles</label>
-                            <input className={`${styles.input} ${styles.RolesInput}`} placeholder='tell us more' type="text" />
-                            <label htmlFor="" className={styles.tagsLabel}>tags</label>
-                            <select className={`${styles.input} ${styles.tagsInput}`} id='industry'>
-                                <option value="">Select relevant tags </option>
-                            </select>
-                        </div>
                         <div className={styles.btns}>
                             <button className={`${styles.cancelBtn} S-BTN`}>Cancel</button>
                             <button className={`${styles.postBtn} P-BTN`}>Post Idea</button>
