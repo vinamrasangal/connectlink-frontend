6dc3cf004242884d8010f2e741626f832c2add14
Malik-kajiji <101195566+Malik-kajiji@users.noreply.github.com>
2023-05-07 11:47:43 +0200

    completed the connections page

---
+++ pages/connections.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from  '../styles/Connections.module.scss';
+import { CiSearch } from 'react-icons/ci';
+import { BiFilter } from 'react-icons/bi';
+import { FiUserCheck } from 'react-icons/fi';
+
+const connections = () => {
+    return (
+        <section className={`container ${styles.container}`}>
+            <h2 className={`${styles.h2} x-large-fs semi-bold dark-gray`}>Connections</h2>
+            <nav className={styles.nav}>
+                <div className={styles.search}>
+                    <span className={`${styles.icon} normal-gray x-large-fs`}>
+                        {CiSearch({})}
+                    </span>
+                    <input 
+                        type="text"
+                        placeholder='Search'
+                        className={`${styles.input} normal-gray medium-fs normal`}    
+                    />
+                </div>
+                <button className={`${styles.filter} S-BTN`}>
+                    <span className={`${styles.icon} dark-gray x-large-fs`}>
+                        {BiFilter({})}
+                    </span>
+                    <p className={`${styles.p}`}>
+                        Filters
+                    </p>
+                </button>
+                <button className={`${styles.Requests} S-BTN`}>
+                    <p className={`${styles.p}`}>
+                    Requests
+                    </p>
+                    <span className={`${styles.icon} dark-gray large-fs`}>
+                        {FiUserCheck({})}
+                    </span>
+                </button>
+            </nav>
+            <article className={styles.users}>
+                <div className={styles.usersHolder}>
+                    <div className={styles.user}>
+                        <img className={styles.image} src="/personal-image.png" alt="" />
+                        <h2 className={`${styles.name} dark-gray semi-bold large-fs`}>Olivia Rhye</h2>
+                        <span className={`${styles.position} medium-fs normal`}>Founder & CEO</span>
+                        <p className={`${styles.desc} light-gray medium-fs light`}>Former co-founder of Opendoor. Early staff at Spotify and Clearbit.</p>
+                    </div>
+                </div>
+                
+            </article>
+        </section>
+    )
+}
+
+export default connections
